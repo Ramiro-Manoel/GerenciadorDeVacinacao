e7f08e5706d13cd93fb87e182f0608008c80c642
Ramiro Manoel <ramiro.manoel77@gmail.com>
2023-11-20 23:42:53 -0300

    metodos de verificacao na classe pessoa

--- src/GerenciadorDeVacinacao/Pessoa.java
+++ src/GerenciadorDeVacinacao/Pessoa.java
@@ -44,6 +44,65 @@ public class Pessoa implements Serializable{
 		return  nome + ", " + cpf + ", " + idade;
 	}
 
+	
+	public boolean verificaCPF(String cpf) {
+        if (verificaNumRepetido(cpf)) {
+            return false;
+        }
+        int[] teste = new int[cpf.length()];
+        for (int i = 0; i < cpf.length(); i++) {
+            teste[i] = Integer.valueOf(cpf.substring(i, i + 1));
+        }
+
+        int primeiroDigito = 0;
+        for (int i = 0; i < (teste.length - 2); i++) {
+            primeiroDigito += teste[i] * (10 - i);
+        }
+
+        int segundoDigito = 0;
+        for (int i = 0; i < (teste.length - 1); i++) {
+            segundoDigito += teste[i] * (11 - i);
+        }
+
+        if ((primeiroDigito * 10) % 11 == teste[9] && (segundoDigito * 10) % 11 == teste[10]) {
+            this.cpf = cpf;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verificaNumRepetido(String cpf) {
+        for (int i = 1; i < cpf.length(); i++) {
+            if (cpf.charAt(i) != cpf.charAt(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean verificaNome(String nome) {
+        if (nome.length() >= 10) {
+            return false;
+        }
+
+        for (int i = 0; i < nome.length(); i++) {
+            if (nome.charAt(i) == ' ') {
+                return false;
+            }
+        }
+        this.nome = nome;
+        return true;
+    }
+
+    public boolean verificaIdade(int idade) {
+        if(idade >= 1 && idade <= 110) {
+            return false;
+        }
+
+        this.idade = idade;
+        return true;
+    }
+	
 	@Override
 	public String toString() {
 		return "Pessoa [nome=" + nome + ", cpf=" + cpf + ", idade=" + idade + "]";
